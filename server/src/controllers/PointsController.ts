8998018f5bc905df6b53504c0435e097e0d997ef
Bruno Rocha <obrunorocha@outlook.com>
2020-06-09 18:14:06 -0300

    Finalizando o projeto

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -3,90 +3,102 @@ import knex from '../database/connection';
 
 class PointsController{
 
-    async index(request: Request, response: Response){
-        const { city, uf, items } = request.query;
+  async index(request: Request, response: Response){
+    const { city, uf, items } = request.query;
 
-        const parsedItems = String(items)
-            .split(',')
-            .map(item => Number(item.trim()));
+    const parsedItems = String(items)
+      .split(',')
+      .map(item => Number(item.trim()));
 
-        const points = await knex('points')
-            .join('point_items', 'points.id', '=', 'point_items.point_id')
-            .whereIn('point_items.item_id', parsedItems)
-            .where('city', String(city))
-            .where('uf', String(uf))
-            .distinct()
-            .select('points.*')
+    const points = await knex('points')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', parsedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('points.*')
 
-        return response.json(points);
-    }
-
-    async show(request: Request, response: Response){
-        const { id } = request.params;
-
-        const point = await knex('points').where('id', id).first();
-
-        if(!point){
-            return response.status(400).json({ message: 'Point not found.' });
+      const serializedPoints = points.map(point => {
+        return {
+          ...point,
+          image_url: `http://192.168.0.104:3333/uploads/${point.image}`,
         }
+      });
 
-        // SELECT * FROM items 
-        // JOIN point_items ON items.id = point_items.item_id 
-        // WHERE point_items.point_id = {id}
+    return response.json(serializedPoints);
+  }
 
-        const items = await knex('items')
-            .join('point_items', 'items.id', '=', 'point_items.item_id')
-            .where('point_items.point_id', id)
-            .select('items.title');
+  async show(request: Request, response: Response){
+    const { id } = request.params;
 
-        return response.json({point, items});
+    const point = await knex('points').where('id', id).first();
+
+    if(!point){
+      return response.status(400).json({ message: 'Point not found.' });
     }
 
-    async create(request: Request, response: Response){
-        const { 
-            name,
-            email,
-            whatsapp, 
-            latitude, 
-            longitude, 
-            city, 
-            uf,
-            items
-        } = request.body;
-    
-        const trx = await knex.transaction();
-    
-        const point = {
-            image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
-            name,
-            email,
-            whatsapp, 
-            latitude, 
-            longitude, 
-            city, 
-            uf 
-        }
+    const serializedPoint = {
+      ...point,
+        image_url: `http://192.168.0.104:3333/uploads/${point.image}`,
+    };
 
-        const insertedIds = await trx('points').insert(point);
-    
-        const point_id = insertedIds[0];
-    
-        const pointItems = items.map((item_id: number) => {
-            return{
-                item_id,
-                point_id,
-            };
-        })
-    
-        await trx('point_items').insert(pointItems);
-    
-        await trx.commit();
+    const items = await knex('items')
+      .join('point_items', 'items.id', '=', 'point_items.item_id')
+      .where('point_items.point_id', id)
+      .select('items.title');
 
-        return response.json({
-            id: point_id,
-           ...point, 
-        });
+
+    return response.json({point: serializedPoint, items});
+  }
+
+  async create(request: Request, response: Response){
+    const { 
+      name,
+      email,
+      whatsapp, 
+      latitude, 
+      longitude, 
+      city, 
+      uf,
+      items
+    } = request.body;
+
+    const trx = await knex.transaction();
+
+    const point = {
+      image: request.file.filename,
+      name,
+      email,
+      whatsapp, 
+      latitude, 
+      longitude, 
+      city, 
+      uf 
     }
+
+    const insertedIds = await trx('points').insert(point);
+
+    const point_id = insertedIds[0];
+
+    const pointItems = items
+      .split(',')
+      .map((item: string) => Number(item.trim()))
+      .map((item_id: number) => {
+        return{
+          item_id,
+          point_id,
+        };
+    })
+  
+    await trx('point_items').insert(pointItems);
+  
+    await trx.commit();
+
+    return response.json({
+      id: point_id,
+      ...point, 
+    });
+  }
 }
 
 export default PointsController;
--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -3,11 +3,13 @@ import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi'
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import axios from 'axios';
-import { LeafletMouseEvent } from 'leaflet'
+import { LeafletMouseEvent } from 'leaflet';
+import api from '../../services/api';
+import Dropzone from '../../components/Dropzone';
 
-import api from '../../services/api'
+import './styles.css';
+
 import logo from '../../assets/logo.svg';
-import './styles.css'
 
 interface Item {
   id: number;
@@ -42,6 +44,7 @@ const CreatePoint = () => {
   const [selectedCity, setSelectedCity] = useState('0');
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   useEffect(() => {
     api.get('items').then(response => {
@@ -120,15 +123,19 @@ const CreatePoint = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items
+    const data = new FormData();
+
+    data.append('name', name);
+    data.append('email', email);
+    data.append('whatsapp', whatsapp);
+    data.append('uf', uf);
+    data.append('city', city);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('items', items.join(','));
+    
+    if(selectedFile){
+      data.append('image', selectedFile)
     }
 
     await api.post('/points', data);
@@ -145,12 +152,14 @@ const CreatePoint = () => {
 
         <Link to="/">
           <FiArrowLeft />
-          Voltar para home
-        </Link>
+        Voltar para home
+      </Link>
       </header>
 
       <form onSubmit={handleSubmit}>
         <h1>Cadastro do <br /> ponto de coleta</h1>
+
+        <Dropzone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
@@ -246,7 +255,7 @@ const CreatePoint = () => {
 
         <button type="submit">
           Cadastrar ponto de coleta
-        </button>
+      </button>
       </form>
     </div>
   )
